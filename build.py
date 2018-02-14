08a4f91b57f55fa5227b4425bee545239d5e9333
Lars Bilke <lars.bilke@ufz.de>
2018-02-14 12:43:03 +0100

    Disable Windows.

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 from conan.packager import ConanMultiPackager
 import copy
 import platform
+from conans.tools import os_info
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs = ["x86_64"])
-    builder.add_common_builds(pure_c=True)
-    builder.run()
+    if not os_info.is_windows:
+        builder = ConanMultiPackager(archs = ["x86_64"])
+        builder.add_common_builds(pure_c=True)
+        builder.run()
